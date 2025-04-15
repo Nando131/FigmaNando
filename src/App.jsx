57d42dcd0f74e1158edf298c68f5d05a9f101b7f
Nando131 <nandoher00@gmail.com>
2025-04-15 14:08:40 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="blank">
+      <div className="container1">
+        <div className='text'>
+          <p className='judul'>Kecerdasan Visual</p>
+          <p className='subtitle'>Lihat dunia dari <br/> lensa yang baru.</p>
+          <p className='deskripsi'>
+            Gunakan <span>kecerdasan visual </span><br/>
+            melalui Kontrol Kamera untuk<br/>
+            langsung mempelajari tentang<br/>
+            semua hal yang Anda liat.<br/>
+            Cukup <span>arahkan iPhone 16 Pro<br/>
+            Anda untuk menemukan lebih </span><br/>
+            atau berinteraksi dengan<br/>
+            sesuatu di depan Anda. Cari<br/>
+            tahu di mana tempat membeli<br/>
+            barang baru yang Anda lihat,<br/>
+            identifikasikan tanaman dan hewan, dan<br/>
+             banyak lagi.
+           </p>
+        </div>
+        <div className='Photo'>
+          <img src="Iphone.png" alt="" />
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
